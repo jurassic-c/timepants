821c33c28699c7ab7082991ec851137646d3df4a
iverticle <iverticle@iv1.(none)>
2013-06-27 20:28:37 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timepants::Application.config.secret_token = 'e42d0f1c8c26ebf46695f5aeea3a1d6cc5a47a272912e7b0b9e1c4ecb212111d3e64ce998c0fa08d2dea1be514816fcf2b66f5f1ce03e17d6c4c640be3cfeaba'
