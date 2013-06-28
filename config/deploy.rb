e3085e34ef19cb08a6a59731caac1b0d64596eb8
iverticle <iverticle@iv1.(none)>
2013-06-27 21:13:12 -0700

    cleaned up the capistrano deploy file a bit

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,8 +7,4 @@ set :scm, :git
 set :user, "jurassic-c"
 
 set :stages ["staging", "production"]
-set :default_stage "staging"
-
-role :web, "iv11"
-role :app, "iv11"
-role :db,  "iv13", :primary => true
+set :default_stage, "staging"
