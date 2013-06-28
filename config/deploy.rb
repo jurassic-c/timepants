5ad6d304f515cbbe1c96472f9dd248a9c6d549b3
iverticle <iverticle@iv1.(none)>
2013-06-27 21:25:38 -0700

    Fixed more bugs with deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,6 @@ set :application, "timepants"
 set :repository,  "https://github.com/jurassic-c/timepants.git"
 
 set :scm, :git
-set :user, "jurassic-c"
 
 set :stages, ["staging", "production"]
 set :default_stage, "staging"
