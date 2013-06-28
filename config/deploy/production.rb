74274f8924cadf4a5783a298720c993f94ea80f6
iverticle <iverticle@iv1.(none)>
2013-06-27 21:07:53 -0700

    Added info for capistrano deployment to staging and development

---
+++ config/deploy/production.rb
@@ -0,0 +1,3 @@
+server "iv11", :app, :web, :db, :primary => true
+set :deploy_to, "/var/www/rails/timepants"
+set :branch, "production"
---
+++ config/deploy/staging.rb
@@ -0,0 +1,2 @@
+server "iv12", :app, :web, :db, :primary => true
+set :deploy_to, "/var/www/timepants"
