eda7284275fa550359ad1b68c4570357c4abd5a6
jurassic-c <colby@nucleartuxedo.com>
2013-06-27 21:38:25 -0700

    Set capistrano to not use sudo when setting up

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,9 @@ set :application, "timepants"
 set :repository,  "https://github.com/jurassic-c/timepants.git"
 
 set :scm, :git
+set :user, "iverticle"
 
 set :stages, ["staging", "production"]
 set :default_stage, "staging"
+
+set :use_sudo, false
